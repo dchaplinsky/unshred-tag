a598273a39689dab6044016d8920b4a8b7effe64
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2014-07-07 02:31:08 +0300

    S3 credentials are now can be set via env variables, old credentials voided

--- load_to_mongo.py
+++ load_to_mongo.py
@@ -9,8 +9,8 @@ from pymongo import ASCENDING
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
-conn = S3Connection('AKIAJUXM6EOCQL5O3NBA',
-                    '+TYHhyjCgW/ZI7xHZg5duXV3khLJ4ijfMMr2Ru8B')
+conn = S3Connection(os.environ.get("aws_access_key_id"),
+                    os.environ.get("aws_secret_access_key"))
 dst_bucket_name = 'kurchenko'
 src_bucket_name = 'kurchenko_pink'
 
@@ -26,41 +26,41 @@ def upload_file(fname):
     return "https://s3.amazonaws.com/%s/%s" % (dst_bucket_name, fname)
 
 if __name__ == '__main__':
-    # dst_bucket.delete_keys(dst_bucket.get_all_keys())
+    dst_bucket.delete_keys(dst_bucket.get_all_keys())
 
-    # shreds.drop()
-    # flt = sys.argv[1]
+    shreds.drop()
+    flt = sys.argv[1]
 
-    # for src_key in src_bucket.get_all_keys():
-    #     if not fnmatch.fnmatch(src_key.key, flt):
-    #         continue
+    for src_key in src_bucket.get_all_keys():
+        if not fnmatch.fnmatch(src_key.key, flt):
+            continue
 
-    #     fname = "/tmp/%s" % os.path.basename(src_key.key)
-    #     src_key.get_contents_to_filename(fname)
+        fname = "/tmp/%s" % os.path.basename(src_key.key)
+        src_key.get_contents_to_filename(fname)
 
-    #     split.out_dir_name = os.path.splitext(os.path.basename(fname))[0]
+        split.out_dir_name = os.path.splitext(os.path.basename(fname))[0]
 
-    #     print("\n\nProcessing file %s from %s" % (fname, split.out_dir_name))
-    #     orig_img, resulting_contours = split.process_file(fname)
+        print("\n\nProcessing file %s from %s" % (fname, split.out_dir_name))
+        orig_img, resulting_contours = split.process_file(fname)
 
-    #     for c in resulting_contours:
-    #         c["_id"] = "%s_%s" % (c["sheet"], c["name"])
-    #         c["order"] = 0
+        for c in resulting_contours:
+            c["_id"] = "%s_%s" % (c["sheet"], c["name"])
+            c["order"] = 0
 
-    #         del(c["simplified_contour"])
-    #         c["contour"] = c["contour"].tolist()
+            del(c["simplified_contour"])
+            c["contour"] = c["contour"].tolist()
 
-    #         imgs = "piece_fname", "features_fname", "piece_in_context_fname"
+            imgs = "piece_fname", "features_fname", "piece_in_context_fname"
 
-    #         for k in imgs:
-    #             if k in c:
-    #                 res = upload_file(c[k])
-    #                 os.remove(c[k])
-    #                 c[k] = res
+            for k in imgs:
+                if k in c:
+                    res = upload_file(c[k])
+                    os.remove(c[k])
+                    c[k] = res
 
-    #         shreds.insert(c)
+            shreds.insert(c)
 
-    # shreds.ensure_index([("name", ASCENDING), ("sheet", ASCENDING)])
+    shreds.ensure_index([("name", ASCENDING), ("sheet", ASCENDING)])
     shreds.ensure_index([("tags", ASCENDING)])
 
     base_tags.drop()
