237cd281a2e1647b156cac1ec15a178e0c37ae50
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2014-10-14 13:49:57 +0200

    Two more test cases for tags ordering in autocomplete

--- tests/tagging.py
+++ tests/tagging.py
@@ -1,6 +1,6 @@
 from flask import url_for
 
-from models import Tags
+from models import Tags, User
 from . import BasicTestCase
 
 
@@ -70,3 +70,60 @@ class TaggingTest(BasicTestCase):
         for tag in Tags.objects(is_base=True):
             self.assertTrue(
                 tag.title.capitalize().encode('unicode-escape') in body)
+
+    def test_user_tags_in_task(self):
+        self.create_user_and_login("user")
+
+        self.client.post(url_for("fixtures.create_shreds"))
+
+        user = User.objects.get(username="user")
+        admin = User.objects.get(username="admin")
+        user_tag = "foobar"
+        another_user_tag = "barfoo"
+
+        Tags.objects.create(title=user_tag, is_base=False,
+                            created_by=user)
+        Tags.objects.create(title=another_user_tag, is_base=False,
+                            created_by=admin)
+
+        user.tags = [user_tag]
+        admin.tags = [another_user_tag]
+        user.save()
+        admin.save()
+
+        res = self.client.get(url_for("next"))
+        self.assert200(res)
+        body = res.get_data(as_text=True)
+
+        self.assertTrue(
+            user_tag.capitalize().encode('unicode-escape') in body)
+        pos = body.index(user_tag.capitalize().encode('unicode-escape'))
+
+        self.assertFalse(
+            another_user_tag.capitalize().encode('unicode-escape') in body)
+
+        for tag in Tags.objects(is_base=True):
+            tag = tag.title.capitalize().encode('unicode-escape')
+            self.assertTrue(tag in body)
+
+            self.assertTrue(body.index(tag) < pos)
+
+    def test_tags_ordering_in_task(self):
+        self.create_user_and_login("user")
+        self.client.post(url_for("fixtures.create_shreds"))
+
+        first_tag = Tags.objects[0]
+        new_first_tag = Tags.objects[1]
+
+        new_first_tag.usages = 100
+        new_first_tag.save()
+
+        res = self.client.get(url_for("next"))
+        self.assert200(res)
+        body = res.get_data(as_text=True)
+
+        self.assertTrue(
+            body.index(first_tag.title.capitalize().encode('unicode-escape')) >
+            body.index(
+                new_first_tag.title.capitalize().encode('unicode-escape'))
+        )
